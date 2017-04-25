f97ac66307ba4d1b9606d9a31a00e2c748c360d6
Dana Franco <turquoise.ilg@hotmail.com>
2017-04-25 16:54:54 -0500

    making changes

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-function allowDrop(event) {
-    event.preventDefault();
-}
-
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
-}
